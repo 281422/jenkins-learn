81abf563def4266fe5f23a5576a3b5ff748e1d41
281422 <55080499+281422@users.noreply.github.com>
2023-07-03 16:26:35 +0800

    Add files via upload
    1

---
+++ jenkins_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# @author  : afeng
+# @time    : 2023/6/30 17:25
+# @function: the script is used to do something.
+# @version : 
+#   V1 
+#
+print("hello world")
+
+
+
